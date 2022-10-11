d8025f247ece98cc1cf70a108fbb450c02fd3294
Jason R. Coombs <jaraco@jaraco.com>
2022-10-10 22:58:39 -0400

    Move config to setup.cfg.

--- abodepy/helpers/constants.py
+++ abodepy/helpers/constants.py
@@ -1,43 +1,9 @@
 """AbodePy constants."""
-import os
-
-MAJOR_VERSION = 0
-MINOR_VERSION = 1
-PATCH_VERSION = 0
-
-__version__ = '{}.{}.{}'.format(MAJOR_VERSION, MINOR_VERSION, PATCH_VERSION)
 
 REQUIRED_PYTHON_VER = (3, 6, 0)
 
-PROJECT_NAME = 'abodepy'
-PROJECT_PACKAGE_NAME = 'jaraco.abode'
-PROJECT_LICENSE = 'MIT'
-PROJECT_AUTHOR = 'Wil Schrader'
-PROJECT_COPYRIGHT = ' 2017, {}'.format(PROJECT_AUTHOR)
-PROJECT_URL = 'https://github.com/jaraco/jaraco.abode'
-PROJECT_EMAIL = 'wilrader@gmail.com'
-PROJECT_DESCRIPTION = ('An Abode alarm Python library '
-                       'running on Python 3.')
-PROJECT_LONG_DESCRIPTION = ('abodepy is an open-source '
-                            'unofficial API for the Abode alarm '
-                            'system with the intention for easy '
-                            'integration into various home '
-                            'automation platforms.')
-if os.path.exists('README.rst'):
-    with open('README.rst') as f:
-        PROJECT_LONG_DESCRIPTION = f.read()
-PROJECT_CLASSIFIERS = [
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python :: 3.6',
-    'Topic :: Home Automation'
-]
-
 PROJECT_GITHUB_USERNAME = 'jaraco'
 PROJECT_GITHUB_REPOSITORY = 'jaraco.abode'
-
-PYPI_URL = 'https://pypi.python.org/pypi/{}'.format(PROJECT_PACKAGE_NAME)
 
 CACHE_PATH = './abode.pickle'
 COOKIES = "cookies"
