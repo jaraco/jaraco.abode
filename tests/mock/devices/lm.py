3c298745c0355f8d098956526894c3f3f7378a77
Jason R. Coombs <jaraco@jaraco.com>
2022-11-08 05:44:53 -0500

    Port lm to native syntax.

--- tests/mock/devices/lm.py
+++ tests/mock/devices/lm.py
@@ -1,4 +1,7 @@
 """Mock Abode LM (Light/Temp/Humidity) Device."""
+
+from .. import dump
+
 DEVICE_ID = 'ZB:eee888801'
 
 TEMP_F = '72 °F'
@@ -19,68 +22,50 @@ def device(
     no_response=False,
 ):
     """PIR mock device."""
-    return (
-        '''
-        {
-          "id":"'''
-        + devid
-        + '''",
-          "type_tag":"device_type.lm",
-          "type":"LM",
-          "name":"Bedroom Temp",
-          "area":"1",
-          "zone":"15",
-          "sort_order":null,
-          "is_window":"",
-          "bypass":"0",
-          "schar_24hr":"0",
-          "sresp_mode_0":"0",
-          "sresp_entry_0":"0",
-          "sresp_exit_0":"0",
-          "sresp_mode_1":"5",
-          "sresp_entry_1":"4",
-          "sresp_exit_1":"0",
-          "sresp_mode_2":"0",
-          "sresp_entry_2":"4",
-          "sresp_exit_2":"0",
-          "sresp_mode_3":"0",
-          "sresp_entry_3":"0",
-          "sresp_exit_3":"0",
-          "version":"LMHT_00.00.03.03TC",
-          "origin":"abode",
-          "deep_link":null,
-          "status_color":"#5cb85c",
-          "faults":{
-             "low_battery":'''
-        + str(int(low_battery))
-        + ''',
-             "tempered":0,
-             "supervision":0,
-             "out_of_order":0,
-             "no_response":'''
-        + str(int(no_response))
-        + '''
-          },
-          "status":"'''
-        + status
-        + '''",
-          "statusEx": "0",
-          "statuses":{
-             "hvac_mode":null,
-             "temperature": "'''
-        + temp
-        + '''",
-             "lux": "'''
-        + lux
-        + '''",
-             "humidity": "'''
-        + humidity
-        + '''"
-          },
-          "status_ex":"",
-          "actions":[],
-          "status_icons":[
-          ],
-          "icon":"assets/icons/unknown.svg"
-       }'''
+    return dump(
+        id=devid,
+        type_tag="device_type.lm",
+        type="LM",
+        name="Bedroom Temp",
+        area="1",
+        zone="15",
+        sort_order=None,
+        is_window="",
+        bypass="0",
+        schar_24hr="0",
+        sresp_mode_0="0",
+        sresp_entry_0="0",
+        sresp_exit_0="0",
+        sresp_mode_1="5",
+        sresp_entry_1="4",
+        sresp_exit_1="0",
+        sresp_mode_2="0",
+        sresp_entry_2="4",
+        sresp_exit_2="0",
+        sresp_mode_3="0",
+        sresp_entry_3="0",
+        sresp_exit_3="0",
+        version="LMHT_00.00.03.03TC",
+        origin="abode",
+        deep_link=None,
+        status_color="#5cb85c",
+        faults=dict(
+            low_battery=int(low_battery),
+            tempered=0,
+            supervision=0,
+            out_of_order=0,
+            no_response=int(no_response),
+        ),
+        status=status,
+        statusEx="0",
+        statuses={
+            "hvac_mode": None,
+            "temperature": temp,
+            "lux": lux,
+            "humidity": humidity,
+        },
+        status_ex="",
+        actions=[],
+        status_icons=[],
+        icon="assets/icons/unknown.svg",
     )
