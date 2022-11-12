2f29683b6af62c2ad3341894f1720ca28df92af0
Jason R. Coombs <jaraco@jaraco.com>
2022-11-12 13:11:59 -0500

    Extract method for enabling color.

--- jaraco/abode/cli.py
+++ jaraco/abode/cli.py
@@ -9,6 +9,7 @@ import contextlib
 import getpass
 
 import keyring
+from jaraco.context import suppress
 
 from . import Client
 from .helpers import constants as CONST
@@ -18,40 +19,42 @@ from .exceptions import AbodeException
 _LOGGER = logging.getLogger('abodecl')
 
 
-def setup_logging(log_level=logging.INFO):
-    """Set up the logging."""
-    logging.basicConfig(level=log_level)
+@suppress(ImportError)
+def enable_color():
+    import colorlog
+
     fmt = "%(asctime)s %(levelname)s (%(threadName)s) " "[%(name)s] %(message)s"
     colorfmt = f"%(log_color)s{fmt}%(reset)s"
     datefmt = '%Y-%m-%d %H:%M:%S'
+
+    handler = logging.getLogger().handlers[0]
+    handler.setFormatter(
+        colorlog.ColoredFormatter(
+            colorfmt,
+            datefmt=datefmt,
+            reset=True,
+            log_colors={
+                'DEBUG': 'cyan',
+                'INFO': 'green',
+                'WARNING': 'yellow',
+                'ERROR': 'red',
+                'CRITICAL': 'red',
+            },
+        )
+    )
+
+
+def setup_logging(log_level=logging.INFO):
+    """Set up the logging."""
+    logging.basicConfig(level=log_level)
 
     # Suppress overly verbose logs from libraries that aren't helpful
     logging.getLogger('requests').setLevel(logging.WARNING)
     logging.getLogger('urllib3').setLevel(logging.WARNING)
     logging.getLogger('aiohttp.access').setLevel(logging.WARNING)
 
-    try:
-        from colorlog import ColoredFormatter
-
-        logging.getLogger().handlers[0].setFormatter(
-            ColoredFormatter(
-                colorfmt,
-                datefmt=datefmt,
-                reset=True,
-                log_colors={
-                    'DEBUG': 'cyan',
-                    'INFO': 'green',
-                    'WARNING': 'yellow',
-                    'ERROR': 'red',
-                    'CRITICAL': 'red',
-                },
-            )
-        )
-    except ImportError:
-        pass
-
-    logger = logging.getLogger('')
-    logger.setLevel(log_level)
+    enable_color()
+    logging.getLogger('').setLevel(log_level)
 
 
 def build_parser():
