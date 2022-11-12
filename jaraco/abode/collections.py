14959bf8fdc53c0485fb8d990b929e6cfd86aa65
Jason R. Coombs <jaraco@jaraco.com>
2022-11-12 13:31:42 -0500

    Allow subkeys to be any mapping.

--- jaraco/abode/collections.py
+++ jaraco/abode/collections.py
@@ -1,10 +1,12 @@
 """Collection routines."""
 
+import collections.abc
+
 
 def update(dct, dct_merge):
     """Recursively merge dct_merge into dct."""
     for key, value in dct_merge.items():
-        if key in dct and isinstance(dct[key], dict):
+        if key in dct and isinstance(dct[key], collections.abc.Mapping):
             dct[key] = update(dct[key], value)
         else:
             dct[key] = value
