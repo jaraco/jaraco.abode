839531cc57ccc0a05ac946bfd08127266d4a9fa8
Jason R. Coombs <jaraco@jaraco.com>
2022-11-12 13:32:53 -0500

    Add type declarations to collections.update.

--- jaraco/abode/collections.py
+++ jaraco/abode/collections.py
@@ -1,12 +1,12 @@
 """Collection routines."""
 
-import collections.abc
+from collections.abc import Mapping, MutableMapping
 
 
-def update(dct, dct_merge):
+def update(dct: MutableMapping, dct_merge: Mapping) -> MutableMapping:
     """Recursively merge dct_merge into dct."""
     for key, value in dct_merge.items():
-        if key in dct and isinstance(dct[key], collections.abc.Mapping):
+        if key in dct and isinstance(dct[key], Mapping):
             dct[key] = update(dct[key], value)
         else:
             dct[key] = value
