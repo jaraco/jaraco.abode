d58545c2e1abf24f464842703b4c4e62eb049f35
Jason R. Coombs <jaraco@jaraco.com>
2022-11-20 16:27:26 -0500

    Add test for dispatcher.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,14 @@
 import sys
+import unittest.mock
 
 import pytest
 
-from jaraco.abode import cli
+from jaraco.abode import cli, client
+
+
+@pytest.fixture
+def mocked_client():
+    return unittest.mock.create_autospec(client.Client)()
 
 
 def test_cli(monkeypatch, capsys):
@@ -11,3 +17,10 @@ def test_cli(monkeypatch, capsys):
         cli.main()
     assert exc.value.code == 0
     assert 'usage' in capsys.readouterr().out
+
+
+class TestDispatcher:
+    def test_dispatch(self):
+        args = cli.build_parser().parse_args([])
+        dispatcher = cli.Dispatcher(mocked_client, args)
+        dispatcher.dispatch()
