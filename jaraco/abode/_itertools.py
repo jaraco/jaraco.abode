d7b0760531b400307f8dbe6560a71db506a33c75
Jason R. Coombs <jaraco@jaraco.com>
2023-02-25 21:23:28 -0500

    Prefer 'only' to 'first' to avoid silently discarding results.

--- jaraco/abode/_itertools.py
+++ jaraco/abode/_itertools.py
@@ -4,11 +4,9 @@ import jaraco.itertools
 import jaraco.functools
 import more_itertools
 
-single = jaraco.functools.compose(
-    more_itertools.first, jaraco.itertools.always_iterable
-)
+single = jaraco.functools.compose(more_itertools.only, jaraco.itertools.always_iterable)
 
 opt_single = jaraco.functools.compose(
-    functools.partial(more_itertools.first, default=None),
+    functools.partial(more_itertools.only, default=None),
     jaraco.itertools.always_iterable,
 )
