db296b97ed3934b131c0f4fe9e0c353dd079250c
Jason R. Coombs <jaraco@jaraco.com>
2024-02-20 10:59:45 -0500

    Rewrite _itertools to fix behavior in single and correct types.
    Replaced use of "only" with "one" where a strict single item is expected. Removed type annotations as the types are now correct with respect to the intended use.

--- jaraco/abode/_itertools.py
+++ jaraco/abode/_itertools.py
@@ -1,16 +1,9 @@
-import functools
-
-from typing import Any, Optional
-
 import jaraco.itertools
 import jaraco.functools
 import more_itertools
 
-single: Any = jaraco.functools.compose(
-    more_itertools.only, jaraco.itertools.always_iterable
-)
+single = jaraco.functools.compose(more_itertools.one, jaraco.itertools.always_iterable)
 
-opt_single: Optional[Any] = jaraco.functools.compose(
-    functools.partial(more_itertools.only, default=None),
-    jaraco.itertools.always_iterable,
+opt_single = jaraco.functools.compose(
+    more_itertools.only, jaraco.itertools.always_iterable
 )
