0635800dbda549dc5f1fedbfe59b7a679b42e8cb
Jason R. Coombs <jaraco@jaraco.com>
2024-09-08 09:33:05 -0400

    Expose `jaraco.abode.config.paths.override` to allow overriding of paths by downstream consumers.
    Closes #36

--- jaraco/abode/config.py
+++ jaraco/abode/config.py
@@ -1,3 +1,15 @@
+"""
+Expose platform paths for the application.
+
+>>> hasattr(paths, 'user_data')
+True
+"""
+
+from __future__ import annotations
+
+from collections.abc import Mapping
+
+import pathlib
 import platformdirs
 
 
@@ -6,8 +18,7 @@ class PlatformDirs(platformdirs.PlatformDirs):  # type: ignore[misc, valid-type]
     Augment PlatformDirs to ensure the data path exists.
 
     >>> dirs = PlatformDirs(appname='Abode', appauthor=False)
-    >>> alt_udp = getfixture('tmp_path') / 'data' / 'dir'
-    >>> vars(dirs).update(user_data_path=alt_udp)
+    >>> dirs.override(data_path=getfixture('tmp_path') / 'data' / 'dir')
     >>> assert dirs.user_data.is_dir()
     """
 
@@ -15,6 +26,16 @@ class PlatformDirs(platformdirs.PlatformDirs):  # type: ignore[misc, valid-type]
     def user_data(self):
         self.user_data_path.mkdir(parents=True, exist_ok=True)
         return self.user_data_path
+
+    def override(self, **kwargs: Mapping[str, pathlib.Path]):
+        """
+        Override the default _path variable.
+
+        >>> dirs = PlatformDirs(appname='Abode', appauthor=False)
+        >>> dirs.override(user_data=pathlib.Path('/tmp/foo'))
+        """
+        for name, path in kwargs.items():
+            vars(self).update({name + '_path': path})
 
 
 paths = PlatformDirs(appname='Abode', appauthor=False)
